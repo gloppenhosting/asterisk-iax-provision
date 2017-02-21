788846eec7d898db39c2199d680d25d78ae57395
Andreas Kruger <ak@pasientsky.no>
2017-02-21 08:53:51 +0100

    Changed codec order

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ domain.run(function() {
                         is_upstream: hostname.indexOf('upstream') > -1 ? 1 : 0,
                         inkeys: 'IAXTrunk',
                         outkeys: 'IAXTrunk',
-                        allow: 'alaw,ulaw',
+                        allow: 'ulaw,alaw',
                         manager_password: 'WLhQWSbPzMvsnq1W',
                         ari_password: 'oPNl8mGtWNBcWS6l'
                             //auth: 'rsa',
